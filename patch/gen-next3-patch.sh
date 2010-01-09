df807b7dfa83d05ce8043418cb755bab841f7b99
Erez Zadok <ezk@cs.sunysb.edu>
2010-01-08 23:30:47 -0500

    generate 2 next3 patches
    One against vanilla, and one against a plain copy of ext3 (just renamed
    next3).  Also, save previous copies of patches, if any.
    
    Signed-off-by: Erez Zadok <ezk@cs.sunysb.edu>

--- patch/gen-next3-patch.sh
+++ patch/gen-next3-patch.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-# generate a Next3 patch
+# generate a Next3 patch against 2.6.31.y,
+# and one against vanilla ext3 (which was just renamed to "next3")
 
 if ! test -d .git || ! test -f fs/next3/next3.h
 then
     echo Please run $0 from the top-level Linux source tree directory
     exit 1
 fi
-file="next3-`date +%F`.patch"
-patchhead=`head -1 .git/logs/refs/remotes/origin/master | awk '{print $2;}'`
-echo "git diff $patchhead.. > $file"
-git diff $patchhead.. > $file || exit $?
+all="next3-`date +%F`-full.patch"
+file="next3-`date +%F`-ext3.patch"
+
+echo "git diff ':/Linux 2.6.31.9'.. > $all"
+mv -f $all $all.old 2> /dev/null
+git diff ':/Linux 2.6.31.9'.. > $all || exit $?
+diffstat $all
+
+echo "git diff ':/patch copy-ext3-to-next3.patch'.. > $file"
+mv -f $file $file.old 2> /dev/null
+git diff ':/patch copy-ext3-to-next3.patch'.. > $file || exit $?
 diffstat $file
