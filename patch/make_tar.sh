e307235c40957054069ea22aa373447d6472bf3a
Amir Goldstein <amir@ctera.com>
2010-03-03 19:45:58 +0200

    strip SNAPSHOT_XXX ifdefs from code when building the next3_snapshot patch

--- patch/make_tar.sh
+++ patch/make_tar.sh
@@ -7,5 +7,5 @@ E2FS=e2fsprogs-1.41.9
 TARFILE=~/snapshot-$DATE.tar.gz
 
 cd ../..
-tar cvfz $TARFILE --exclude=.svn snapshot/{bin,docs,$LINUX,$E2FS,patch/*.{sh,sed}}
+tar cvfz $TARFILE --exclude=.svn snapshot/{bin,docs,$LINUX,$E2FS,patch/Makefile,patch/*.{sh,sed,c}}
 
