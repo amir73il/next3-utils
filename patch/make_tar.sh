b097646faa8237fad6c7714f1013d1c3a7e4af10
Amir Goldstein <amir@ctera.com>
2010-03-16 10:52:05 +0200

    moving from ext3 and back wiki

--- patch/make_tar.sh
+++ patch/make_tar.sh
@@ -3,10 +3,11 @@
 DATE=$(date +%d%m%y)
 LINUX=linux-2.6.31.11
 E2FS=e2fsprogs-1.41.9
-NEXT3=next3-1.0.0
+NEXT3=next3-1.0.9
 
 TARFILE=~/$NEXT3-$DATE.tar.gz
 
 cd ../..
+ln -sf next3 $NEXT3
 tar cvfz $TARFILE --exclude=.svn $NEXT3/{bin,docs,$LINUX,$E2FS,patch/Makefile,patch/*.{sh,c}}
 
