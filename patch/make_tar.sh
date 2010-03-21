b4629935873ca088f1e5dc1b51600e5c6b655591
Amir Goldstein <amir@ctera.com>
2010-03-21 16:22:19 +0200

    update wiki and build release package

--- patch/make_tar.sh
+++ patch/make_tar.sh
@@ -3,11 +3,11 @@
 DATE=$(date +%d%m%y)
 LINUX=linux-2.6.31.11
 E2FS=e2fsprogs-1.41.9
-NEXT3=next3-1.0.9
+PKG=snapshot
 
-TARFILE=~/$NEXT3-$DATE.tar.gz
+TARFILE=~/$PKG-$DATE.tar.gz
 
 cd ../..
-test -e $NEXT3 || ln -sf next3 $NEXT3
-tar cvhfz $TARFILE --exclude=.svn $NEXT3/{bin,docs,$LINUX,$E2FS,patch/Makefile,patch/*.{sh,c}}
+test -e $PKG || ln -sf next3 $PKG
+tar cvhfz $TARFILE --exclude=.svn $PKG/{bin,docs,$LINUX,$E2FS,patch/Makefile,patch/*.{sh,c}}
 
